1a5a2382ee2334f355228ccecc33345fdcf45e78
Vladimir Istomin <istomin2798@gmail.com>
2021-04-17 10:09:36 +0300

    Add gitignore and connect to MongoDB Atlas

--- index.js
+++ index.js
@@ -1,40 +1,106 @@
 const express = require('express');
-const { getDate } = require('./public/scripts/getDate');
+const mongoose = require('mongoose');
+const _ = require('lodash');
 
+// Express configuration
 
 const app = express();
 app.use(express.urlencoded());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-const tasks = [];
-const educationTasks = [];
+// Mongodb configuration
+
+mongoose.connect('mongodb+srv://vladimiristomin:HigherSchool2020!@todolist.xpdaq.mongodb.net/Tasks?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false});
+
+const taskSchema = mongoose.Schema({
+    name: String
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+const taskListSchema = mongoose.Schema({
+    name: String,
+    tasks: [taskSchema]
+});
+
+const TaskList = mongoose.model('List', taskListSchema);
+
+
+async function createBasicTasks(listName) {
+    const task1 = new Task({name: "Click + to add new task"});
+    const task2 = new Task({name: "<-- Click here to delete the task"});
+    const task3 = new Task({name: "Finally, plan your day!"});
+    const tasks = [task1, task2, task3];
+    const taskList = new TaskList({name: listName, tasks});
+    await taskList.save();
+    return
+}
+
 
 app.get('/', (req, res) => {
-    const date = getDate();
+    const listName = 'Today';
 
-    res.render('index', {plannerType: date, tasks: tasks});
-})
+    TaskList.findOne({name: listName}, async (err, list) => {
+        if (!list) {
+            await createBasicTasks(listName)
+            res.redirect('/');
+        } else {
+            res.render('index', {listName, tasks: list.tasks});
+        }        
+    });
+});
+
+app.get('/:anotherList', (req, res) => {
+    const listName = _.capitalize(req.params.anotherList);
+
+    TaskList.findOne({name: listName}, async (err, list) => {
+        if (!list) {
+            await createBasicTasks(listName);
+            res.redirect(`/${listName}`);
+        } else {
+            res.render('index', {listName, tasks: list.tasks})
+        }
+    });
+
+});
 
 app.post('/', (req, res) => {
-    const newTask = req.body.task;
+    let newTask = req.body.task;
 
     if (req.body.task === '') {
         return;
     }
 
-    if (req.body.addTask === 'education') {
-        educationTasks.push(newTask);
-        res.redirect('/education');
-    } else {
-        tasks.push(newTask);
-        res.redirect('/');
-    }
-})
+    newTask = new Task({name: newTask});
 
-app.get('/education', (req, res) => {
-    res.render('index', {plannerType: 'education', tasks: educationTasks});
-})
+    const listName = req.body.listName;
+
+    if (listName === 'Today') {
+        TaskList.findOne({name: 'Today'}, async (err, list) => {
+            list.tasks.push(newTask);
+            await list.save();
+            res.redirect('/');
+        });
+    } else {
+        TaskList.findOne({name: listName}, async (err, list) => {
+            list.tasks.push(newTask);
+            await list.save();
+            res.redirect(`/${listName}`);
+        });
+    }
+});
+
+app.post('/delete', (req, res) => {
+    const listName = req.body.listName;
+    const taskId = req.body.taskId;
+
+    TaskList.findOneAndUpdate({name: listName}, {$pull: {tasks: {_id: taskId}}}, (err, list) => {
+        if (!err) {
+            res.redirect(`/${listName}`);
+        }
+    });
+});
 
 
 app.listen(process.env.PORT || 3000, () => console.log('The application has been started!'));
